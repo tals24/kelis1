271adb6199046e4b6c8cab07338841d603a40aa8
gabi <gabi.erez@tufin.com>
2024-04-16 20:04:27 +0300

    gabi 2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import {ChartsComponent} from "./charts/charts.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about vvvv
-    </a>
+    <app-charts></app-charts>
   `,
+  imports: [
+    ChartsComponent
+  ]
 })
 export class App {
   name = 'Angular';
