fba42205bb73d86fee6fcb774d92f47e7590c0c0
gabi <gabi.erez@tufin.com>
2024-04-17 07:06:08 +0300

    charts and tabs

--- src/charts/charts.component.ts
+++ src/charts/charts.component.ts
@@ -2,26 +2,28 @@ import { Component } from '@angular/core';
 import {MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
 import {CanvasComponent} from "./canvas/canvas.component";
 import {MatButtonModule} from "@angular/material/button";
+import {ColumnsComponent} from "./columns/columns.component";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [MatButtonModule,MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  imports: [MatButtonModule, MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, MatGridTile, MatGridList, CanvasComponent, ColumnsComponent],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css'
 })
 export class ChartsComponent {
   constructor(public dialog: MatDialog) {}
 
-  // openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-  //   this.dialog.open(CanvasLineComponent, {
-  //     width: '900px',
-  //     height: '440px',
-  //     minWidth:'900px',
-  //     enterAnimationDuration,
-  //     exitAnimationDuration,
-  //   });
-  // }
+  openColumnsComponent(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(ColumnsComponent, {
+      width: '900px',
+      height: '440px',
+      minWidth:'900px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 
   openDialogCanvasLine(enterAnimationDuration: string, exitAnimationDuration: string): void {
     this.dialog.open(CanvasComponent, {
